5ac6f8f19721157d9be54051d4f59355eecc72a7
Raymond Hunce <hunce18c@gmail.com>
2021-03-06 11:08:39 -0500

    Implement improved three-number-sum solution

--- data-structures-and-algorithms/medium/three-number-sum.js
+++ data-structures-and-algorithms/medium/three-number-sum.js
@@ -2,7 +2,7 @@
 Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. The function should find all triplets in the array that sum up to the target sum and return a 2D array of all these triplets. The numbers in each triplet should be ordered in ascending order, and the triplets themselves should be ordered in ascending order with respect to the numbers they hold. If no three numbers sum up to the target sum, the function should return an empty array.
 */
 
-// Naive/Brute Force Solution
+// Naive/Brute Force Solution (35 minutes)
 function threeNumberSum(array, targetSum) {
 	let triplets = [];
 	for (let i = 0; i < array.length - 2; i++) {
@@ -30,6 +30,48 @@ function threeNumberSum(array, targetSum) {
 	return triplets;
 }
 // Time Complexity: O(n^3)
+// Space Complexity: O(n)
+
+// Improved Solution (1 hr 20 min)
+function threeNumberSum(array, targetSum) {
+  array.sort((a, b) => { return a - b })
+	let triplets = [];
+	let sumCache = {};
+	for (let i = 1; i < array.length - 1; i++) {
+		for (let j = i + 1; j < array.length; j++) {
+			let currentKey = `${array[i]}+${array[j]}`;
+			let currentValue = array[i] + array[j];
+			sumCache[currentKey] = currentValue;
+		}
+	}
+	for (let i = 0; i < array.length - 2; i++) {
+		for (let key in sumCache) {
+		  if (key.split('+').map( e => { return parseInt(e); } ).indexOf(array[i]) !== -1) {
+			  delete sumCache[key];
+		  } else {
+			  if (array[i] + sumCache[key] === targetSum) {
+          let newTriplet = key.split('+').map( e => { return parseInt(e); } );
+          newTriplet.push(array[i])
+          newTriplet.sort((a, b) => { return a - b });
+          triplets.push(newTriplet);
+			  }
+		  }
+		}
+	}
+	triplets.sort((a, b) => {
+	  if (a[0] !== b[0]) {
+		  return a[0] - b[0];
+		} else if (a[1] !== b[1]) {
+		  return a[1] - b[1];
+		} else if (a[2] !== b[2]) {
+		  return a[2] - b[2];
+		} else {
+		  return;
+		}
+	})
+	return triplets;
+}
+// Time Complexity: O(n^2)
 // Space Complexity: O(n)
 
 // Test Case
