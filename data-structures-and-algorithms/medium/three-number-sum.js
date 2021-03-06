b5473cbd610a7287efb16e6e2423083997399fdb
Raymond Hunce <hunce18c@gmail.com>
2021-03-06 14:34:09 -0500

    Implement three-number-sum optimal solution

--- data-structures-and-algorithms/medium/three-number-sum.js
+++ data-structures-and-algorithms/medium/three-number-sum.js
@@ -74,6 +74,33 @@ function threeNumberSum(array, targetSum) {
 // Time Complexity: O(n^2)
 // Space Complexity: O(n)
 
+// Optimal Solution
+function threeNumberSum(array, targetSum) {
+  array.sort((a, b) => {
+    return a - b;
+  })
+  let triplets = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    let leftPointerLocation = i + 1;
+    let rightPointerLocation = array.length - 1;
+    while (leftPointerLocation < rightPointerLocation) {
+      let currentSum = array[i] + array[leftPointerLocation] + array[rightPointerLocation];
+      if (currentSum === targetSum) {
+        triplets.push([array[i], array[leftPointerLocation], array[rightPointerLocation]])
+        leftPointerLocation += 1;
+        rightPointerLocation -= 1;
+      } else if (currentSum > targetSum) {
+        rightPointerLocation -= 1;
+      } else if (currentSum < targetSum) {
+        leftPointerLocation += 1;
+      }
+    }
+  }
+  return triplets;
+}
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+
 // Test Case
 const testArray = [12, 3, 1, 2, -6, 5, -8, 6];
 const testTargetSum = 0;
