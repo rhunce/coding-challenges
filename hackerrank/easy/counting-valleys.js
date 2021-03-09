f73175558c7a34629cb43bc31570861401fd05ae
Raymond Hunce <hunce18c@gmail.com>
2021-03-08 20:09:46 -0500

    Implement solution for counting-valleys

---
+++ hackerrank/easy/counting-valleys.js
@@ -0,0 +1,36 @@
+/*
+An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly STEP steps, for every step it was noted if it was an uphill, U, or a downhill, D step. Hikes always start and end at sea level, and each step up or down represents a 1 unit change in altitude. We define the following terms:
+
+A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+Function Description
+
+Complete the countingValleys function in the editor below.
+countingValleys has the following parameter(s):
+int steps: the number of steps on the hike
+string path: a string describing the path
+
+Returns
+
+int: the number of valleys traversed
+*/
+
+function countingValleys(steps, path) {
+  let currentAltitude = 0;
+  let numberOfValleys = 0;
+  for (let i = 0; i < path.length; i++) {
+    if (path[i] === 'U') {
+        currentAltitude++;
+    } else {
+      if (currentAltitude === 0) {
+        numberOfValleys++;
+        currentAltitude--;
+      } else {
+        currentAltitude--;
+      }
+    }
+  }
+  return numberOfValleys;
+}
