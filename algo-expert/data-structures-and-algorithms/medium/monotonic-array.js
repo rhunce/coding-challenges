090dedcc8d52dad5d67ffc5b1fc1cad6b1968ebc
Raymond Hunce <hunce18c@gmail.com>
2021-03-10 14:22:53 -0500

    Implement source solution for monotonic-array

--- algo-expert/data-structures-and-algorithms/medium/monotonic-array.js
+++ algo-expert/data-structures-and-algorithms/medium/monotonic-array.js
@@ -5,7 +5,7 @@ Non-increasing elements aren't necessarily exclusively decreasing; they simply d
 Note that empty array and array of one element are monotonic
 */
 
-// My solution
+// My solution (20 min)
 function isMonotonic(array) {
   if (array.length <= 1) {
 		return true;
@@ -23,7 +23,7 @@ function isMonotonic(array) {
 // Time Complexity: O(n)
 // Space Complexity: O(n)
 
-// My optimized solution
+// My optimized solution (11 min)
 function isMonotonic(array) {
 	if (array.length <= 1) {
 		return true;
@@ -47,6 +47,34 @@ function isMonotonic(array) {
 	} else {
 		return false;
 	}
+}
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// Source solution
+function isMonotonic(array) {
+  if (array.length <= 2) {
+    return true;
+  }
+  let direction = array[1] - array[0];
+  for (let i = 2; i < array.length; i++) {
+    if (direction === 0) {
+      direction = array[i] - array[i - 1];
+      continue;
+    }
+    if (breaksDirection(direction, array[i - 1], array[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function breaksDirection(direction, previousInt, currentInt) {
+  const difference = currentInt - previousInt;
+  if (direction > 0) {
+    return difference < 0;
+  }
+  return difference > 0;
 }
 // Time Complexity: O(n)
 // Space Complexity: O(1)
