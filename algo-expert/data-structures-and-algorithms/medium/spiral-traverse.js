46a0ee8827169ffb5e0eddc2fd9b00866dceae3c
Raymond Hunce <hunce18c@gmail.com>
2021-03-11 13:15:31 -0500

    Implement optimal source solution for spiral-traverse

--- algo-expert/data-structures-and-algorithms/medium/spiral-traverse.js
+++ algo-expert/data-structures-and-algorithms/medium/spiral-traverse.js
@@ -60,3 +60,53 @@ function spiralTraverse(array) {
 }
 // Time Complexity: O(n^2)
 // Space Complexity: O(n)
+
+// Optimal source solution
+function spiralTraverse(array) {
+  const result = [];
+  let startRow = 0;
+  let endRow = array.length - 1;
+  let startCol = 0;
+  let endCol = array[0].length - 1;
+
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let col = startCol; col <= endCol; col++) {
+      result.push(array[startRow][col]);
+    }
+
+    for (let row = startRow + 1; row <= endRow; row++) {
+      result.push(array[row][endCol]);
+    }
+
+    for (let col = endCol - 1; col >= startCol; col--) {
+      // Handle the edge case when there's a single row
+      // in the middle of the matrix. In this case, we don't
+      // want to double-count the values in this row, which
+      // we've already counted in the first for loop above.
+      if (startRow === endRow) {
+        break;
+      }
+      result.push(array[endRow][col]);
+    }
+
+    for (let row = endRow - 1; row > startRow; row--) {
+      // Handle the edge case when there's a single column
+      // in the middle of the matrix. In this case, we don't
+      // want to double-count the values in this column, which
+      // we've already counted in the second for loop above.
+      if (startCol === endCol) {
+        break;
+      }
+      result.push(array[row][startCol]);
+    }
+
+    startRow++;
+    endRow--;
+    startCol++;
+    endCol--;
+  }
+
+  return result;
+}
+// Time Complexity: O(n)
+// Space Complexity: O(n)
