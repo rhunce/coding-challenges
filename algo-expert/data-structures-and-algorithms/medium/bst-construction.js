e613026e88b89d9e1289970cdcf9bfffd0b5d577
Raymond Hunce <hunce18c@gmail.com>
2021-03-15 21:11:14 -0400

    Implement source solution 2 for bst-construction

--- algo-expert/data-structures-and-algorithms/medium/bst-construction.js
+++ algo-expert/data-structures-and-algorithms/medium/bst-construction.js
@@ -13,7 +13,7 @@ Each BST node has an integer value, a left child node, and a right child node. A
   - its children are either valid BST nodes themselves or null.
 */
 
-// SOURCE SOLUTION 1
+// SOURCE SOLUTION 1 (might be an error in here somewhere; check against algoexpert tests)
 class BST {
   constructor(value) {
     this.value = value;
@@ -104,3 +104,100 @@ class BST {
     }
   }
 }
+
+// SOURCE SOLUTION 2
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    let currentNode = this;
+    while (true) {
+      if (value < currentNode.value) {
+        if (currentNode.left === null) {
+          currentNode.left = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        if (currentNode.right === null) {
+          currentNode.right = new BST(value);
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
+    return this;
+  }
+  // AVERAGE: O(log(n)) time | O(1) space
+  // WORST: O(n) time | O(1) space
+
+  constains(value) {
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+  // AVERAGE: O(log(n)) time | O(1) space
+  // WORST: O(n) time | O(1) space
+
+  remove(value, parentNode = null) {
+    let currentNode = this;
+    while (currentNode !== null) {
+      if (value < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else {
+        if (currentNode.left !== null && currentNode.right !== null) {
+          currentNode.value = currentNode.right.getMinValue();
+          currentNode.right.remove(currentNode.value, currentNode);
+        } else if (parentNode === null) {
+          if (currentNode.left !== null) {
+            currentNode.value = currentNode.left.value;
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+          } else if (currentNode.right !== null) {
+            currentNode.value = currentNode.right.value;
+            currentNode.left = currentNode.right.left;
+            currentNode.right = currentNode.right.right;
+          } else {
+            // This is a single-node tree; do nothing.
+          }
+        } else if (parentNode.left === currentNode) {
+          parentNode.left = currentNode.left !== null ? currentNode.left : currentNode.right;
+        } else if (parentNode.right === currentNode) {
+          parentNode.right = currentNode.left !== null ? currentNode.left : currentNode.right;
+        }
+        break;
+      }
+    }
+    return this;
+  }
+  // AVERAGE: O(log(n)) time | O(1) space
+  // WORST: O(n) time | O(1) space
+
+  getMinValue() {
+    let currentNode = this;
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
+  }
+}
+
+
