bcda618dd64484ef7c3b27c041cd7e27c03ccf88
Raymond Hunce <hunce18c@gmail.com>
2021-03-17 19:34:49 -0400

    Implement source solution for validate-bst

--- algo-expert/data-structures-and-algorithms/medium/validate-bst.js
+++ algo-expert/data-structures-and-algorithms/medium/validate-bst.js
@@ -40,3 +40,30 @@ function validateBst(tree) {
 
   return true;
 }
+
+// SOURCE SOLUTION
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function validateBst(tree) {
+  return validateBstHelper(tree, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY)
+}
+// Time Complexity: O(n)
+// Space Complexity: O(d)
+// d = depth of tree
+
+function validateBstHelper(tree, minValue, maxValue) {
+  if (tree === null) {
+    return true;
+  }
+  if (tree.value < minValue || tree.value >= maxValue) {
+    return false;
+  }
+  const leftIsValid = validateBstHelper(tree.left, minValue, tree.value);
+  return leftIsValid && validateBstHelper(tree.right, tree.value, maxValue);
+}
