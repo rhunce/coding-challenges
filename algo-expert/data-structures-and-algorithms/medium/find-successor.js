74a11727cbd39b56e9755dbde55c53f07ce344a2
Raymond Hunce <hunce18c@gmail.com>
2021-03-24 14:36:26 -0400

    Implement brute force solution for find-successor

---
+++ algo-expert/data-structures-and-algorithms/medium/find-successor.js
@@ -0,0 +1,42 @@
+/*
+Write a function that takes in a Binary Tree (where nodes have an additional pointer to their parent node) as well as a node contained in that tree and returns the given node's successor.
+
+A node's successor is the next node to be visited (immediately after the given node) when travering its tree using the in-order tree-traversal technique. A node has no successor if it's the last node to be visited in the in-order traversal.
+
+If a node has no successor, your function should return null.
+*/
+
+// This is an input class. Do not edit.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+}
+
+// BRUTE FORCE SOLUTION (12 min)
+function findSuccessor(tree, node) {
+  let nodes = [];
+  nodeCollector(tree, nodes);
+  for (let i = 0; i < nodes.length; i++) {
+    if (node === nodes[i]) {
+      if (nodes[i + 1] !== null) {
+        return nodes[i + 1];
+      }
+    }
+  }
+  return null;
+}
+
+function nodeCollector(binaryTree, collection) {
+  if (binaryTree === null) {
+    return;
+  }
+  nodeCollector(binaryTree.left, collection);
+  collection.push(binaryTree);
+  nodeCollector(binaryTree.right, collection);
+}
+// Time Complexity: O(n)
+// Space Complexity: O(n)
