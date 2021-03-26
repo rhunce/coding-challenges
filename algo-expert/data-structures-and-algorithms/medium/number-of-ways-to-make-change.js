c809ea3514fae0fc20582a1fd153ac72a8ca280b
Raymond Hunce <hunce18c@gmail.com>
2021-03-26 13:12:39 -0400

    Implement source solution for number-of-ways-to-make-change

--- algo-expert/data-structures-and-algorithms/medium/number-of-ways-to-make-change.js
+++ algo-expert/data-structures-and-algorithms/medium/number-of-ways-to-make-change.js
@@ -2,6 +2,7 @@
 Given an array of distinct positive integers representing coin denominations and a single non-negative integer n representing a target amount of money, write a function that returns the number of ways to make change for that target amount using the given coin denominations. Note that an unlimited amount of coins is at your disposal.
 */
 
+// MY SOLUTION
 class CountInfo {
   constructor(count) {
     this.count = count;
@@ -28,3 +29,20 @@ function numberOfWaysToMakeChange(n, denoms) {
 }
 // Time Complexity: O(?)
 // Space Complexity: O(n)
+
+// SOURCE SOLUTION
+function numberOfWaysToMakeChange(n, denoms) {
+  const ways = new Array(n + 1).fill(0);
+  ways[0] = 1;
+  for (let denom of denoms) {
+    for (let amount = 1; amount < n + 1; amount++) {
+      if (denom <= amount) {
+        ways[amount] += ways[amount - denom];
+      }
+    }
+  }
+  return ways[n];
+}
+// Time Complexity: O(nd)
+// Space Complexity: O(n)
+// n = target amount and d = number of coin denominations.
