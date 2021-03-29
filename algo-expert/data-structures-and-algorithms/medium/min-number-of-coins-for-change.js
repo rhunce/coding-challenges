d8db862573eedada8c7e50c49509e23a179db093
Raymond Hunce <hunce18c@gmail.com>
2021-03-29 14:16:41 -0400

    Implement source solution for min-number-of-coins-for-change

--- algo-expert/data-structures-and-algorithms/medium/min-number-of-coins-for-change.js
+++ algo-expert/data-structures-and-algorithms/medium/min-number-of-coins-for-change.js
@@ -6,4 +6,50 @@ Note that you have access to an unlimited amount of coins. In other words, if th
 If it's impossible to make change for the target amount, return -1.
 */
 
-// MY PARTIALLY CORRECT SOLUTION
+// MY PARTIALLY CORRECT SOLUTION (50 min)
+class CoinInfo {
+  constructor(currentCoinCount, minCoinCount) {
+    this.currentCoinCount = currentCoinCount;
+    this.minCoinCount = minCoinCount;
+  }
+}
+
+function minNumberOfCoinsForChange(n, denoms) {
+  let coinInfo = new CoinInfo(0, -1);
+  const coinCounter = function(targetAmount, denoms) {
+    if (targetAmount < 0) {
+      return;
+    }
+    if (targetAmount === 0) {
+      if (coinInfo.minCoinCount === -1 || coinInfo.currentCoinCount < coinInfo.minCoinCount) {
+        coinInfo.minCoinCount = coinInfo.currentCoinCount;
+        coinInfo.currentCoinCount = 0;
+        return;
+      } else {
+        coinInfo.currentCoinCount = 0;
+        return;
+      }
+    }
+    for (let i = 0; i < denoms.length; i++) {
+      let newAmount = targetAmount - denoms[i];
+      coinInfo.currentCoinCount++;
+      coinCounter(newAmount, denoms);
+    }
+  }
+  coinCounter(n, denoms, coinInfo);
+  return coinInfo.minCoinCount;
+}
+
+// SOURCE SOLUTION
+function minNumberOfCoinsForChange(n, denoms) {
+  const numOfCoins = new Array(n + 1).fill(Number.POSITIVE_INFINITY);
+  numOfCoins[0] = 0;
+  for (const denom of denoms) {
+    for (let amount = 0; amount < numOfCoins.length; amount++) {
+      if (denom <= amount) {
+        numOfCoins[amount] = Math.min(numOfCoins[amount], numOfCoins[amount - denom] + 1);
+      }
+    }
+  }
+  return numOfCoins[n] !== Number.POSITIVE_INFINITY ? numOfCoins[n] : -1;
+}
