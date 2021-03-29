277fdf0a6740faf2ed1faac36ac4cd66dbc1d7e1
Raymond Hunce <hunce18c@gmail.com>
2021-03-29 14:11:12 -0400

    Implement partial solution for min-number-of-coins-for-change

--- algo-expert/data-structures-and-algorithms/medium/number-of-ways-to-make-change.js
+++ algo-expert/data-structures-and-algorithms/medium/number-of-ways-to-make-change.js
@@ -2,7 +2,7 @@
 Given an array of distinct positive integers representing coin denominations and a single non-negative integer n representing a target amount of money, write a function that returns the number of ways to make change for that target amount using the given coin denominations. Note that an unlimited amount of coins is at your disposal.
 */
 
-// MY SOLUTION
+// MY SOLUTION (45 min)
 class CountInfo {
   constructor(count) {
     this.count = count;
