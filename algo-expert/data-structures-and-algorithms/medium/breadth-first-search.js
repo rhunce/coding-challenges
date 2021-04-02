ea2d71184d1e944da353a65df8492772a052ebe5
Raymond Hunce <hunce18c@gmail.com>
2021-04-02 13:07:22 -0400

    Implement solution for breadth-first-search

---
+++ algo-expert/data-structures-and-algorithms/medium/breadth-first-search.js
@@ -0,0 +1,34 @@
+/*
+You're given a Node class that has a name and an array of optional children nodes. When put together, nodes form an acyclic tree-like structure.
+
+Implement the breadthFirstSearch method on the Node class, which takes in an empty array, traverses the tree using the Breadth-First Search approach (specifically navigating the tree from left to right), stores all of the nodes' names in the input array, and returns it.
+*/
+
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  // MY SOLUTION (23 min)
+  breadthFirstSearch(array = [], root = this, queue = []) {
+    array.push(root.name);
+    if (root.children.length > 0) {
+      for (let child of root.children) {
+        queue.push(child);
+      }
+    }
+    if (queue.length > 0) {
+      let nextNode = queue.shift();
+      nextNode.breadthFirstSearch(array, nextNode, queue);
+    }
+    return array;
+  }
+  // Time Complexity: O(n)
+  // Space Complexity: O(n)
+}
