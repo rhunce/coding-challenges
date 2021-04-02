954d904c0d92bea9c41abb98e0a13c5ab0c9587c
Raymond Hunce <hunce18c@gmail.com>
2021-04-02 10:35:44 -0400

    Implement solution attempt to single-cycle check

---
+++ algo-expert/data-structures-and-algorithms/medium/single-cycle-check.js
@@ -0,0 +1,33 @@
+/*
+You're given an array of integers where each integer represents a jump of its value in the array. For instance, the integer 2 represents a jump of two indices forward in the array; the integer -3 represents a jump of three indices backward in the array.
+
+If a jump spills past the array's bounds, it wraps over to the other side. For instance, a jump of -1 at index 0 brings us to the last index in the array. Similarly, a jump of 1 at the last index in the array brings us to index 0.
+
+Write a function that return a boolean representing whether the jumps in the array form a single cycle. A single cycle occurs if, starting at any index in the array and following the jumps, every elements in the array is visited exactly once before landing back on the starting index.
+*/
+
+// MY INCOMPLETE SOLUTION
+function hasSingleCycle(array) {
+  let nextIndex = array[0];
+  let currentIndex = 0;
+  array[0] = true;
+  let count = array.length - 1;
+  while (count > 0) {
+    if (array[nextIndex] === true) {
+      return false;
+    }
+    currentIndex = nextIndex;
+    // LIKELY NEED TO MAKE ADJUSTMENTS TO THE BELOW IF-ELSE BLOCK
+    if (array[currentIndex] + currentIndex < 0) {
+      nextIndex = (Math.abs(array[currentIndex]) % array.length) + currentIndex;
+    } else {
+      nextIndex = nextIndex + array[nextIndex];
+    }
+    array[currentIndex] = true;
+    count--;
+  }
+  if (nextIndex === 0) {
+    return true;
+  }
+  return false;
+}
