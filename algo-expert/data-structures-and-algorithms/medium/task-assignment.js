382e6aa974f510ac32187b67aab07afe2cb77420
Raymond Hunce <hunce18c@gmail.com>
2021-04-08 14:22:37 -0400

    Implement source solution for task-assignment

--- algo-expert/data-structures-and-algorithms/medium/task-assignment.js
+++ algo-expert/data-structures-and-algorithms/medium/task-assignment.js
@@ -43,5 +43,44 @@ function findMaxAndMinTasks(tasks, optimalTaskAssignments) {
     findMaxAndMinTasks(tasks, optimalTaskAssignments);
   }
 }
-// Time Complexity: O(n)
+// Time Complexity: O(n) ...or is it O(n^2)?
+// Space Complexity: O(n)
+
+// SOURCE SOLUTION (I PREFER MINE, ABOVE)
+function taskAssignment(k, tasks) {
+  const pairedTasks = [];
+  const taskDurationToIndices = getTaskDurationToIndices(tasks);
+
+  const sortedTasks = [...tasks].sort((a, b) => a - b);
+  for (let idx = 0; idx < k; idx++) {
+    const task1Duration = sortedTasks[idx];
+    const indicesWithTask1Duration = taskDurationToIndices[task1Duration];
+    const task1Index = indicesWithTask1Duration.pop();
+
+    const task2SortedIndex = tasks.length - 1 - idx;
+    const task2Duration = sortedTasks[task2SortedIndex];
+    const indicesWithTask2Duration = taskDurationToIndices[task2Duration];
+    const task2Index = indicesWithTask2Duration.pop();
+
+    pairedTasks.push([task1Index, task2Index]);
+  }
+
+  return pairedTasks;
+}
+
+function getTaskDurationToIndices(tasks) {
+  const taskDurationToIndices = {};
+
+  for (let idx = 0; idx < tasks.length; idx++) {
+    const taskDuration = tasks[idx];
+    if (taskDuration in taskDurationToIndices) {
+      taskDurationToIndices[taskDuration].push(idx);
+    } else {
+      taskDurationToIndices[taskDuration] = [idx];
+    }
+  }
+
+  return taskDurationToIndices;
+}
+// Time Complexity: O(n log n)
 // Space Complexity: O(n)
