25449783f9903f3f467ee523d9efd08cb121fb46
Raymond Hunce <hunce18c@gmail.com>
2021-04-10 10:37:09 -0400

    Implement source solution for min-heap-construction

--- algo-expert/data-structures-and-algorithms/medium/min-heap-construction.js
+++ algo-expert/data-structures-and-algorithms/medium/min-heap-construction.js
@@ -9,7 +9,7 @@ Implement a MinHeap class that supports:
 Note that the heap should be represented in the form of an array.
 */
 
-// MY INITIAL ATTEMPT
+// MY INITIAL ATTEMPT (45 min)
 class MinHeap {
   constructor(array) {
     this.heap = this.buildHeap(array);
@@ -99,5 +99,84 @@ class MinHeap {
     let rootValue = this.heap[0];
     this.heap[0] = this.heap[this.heap.length - 1];
     this.heap[this.heap.length - 1] = rootValue;
+  }
+}
+
+// SOURCE SOLUTION
+class MinHeap {
+  constructor(array) {
+    this.heap = this.buildHeap(array);
+  }
+
+  buildHeap(array) {
+    const firstParentIdx = Math.floor(array.length - 1 - 1 / 2);
+    for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+      this.siftDown(currentIdx, array.length - 1, array);
+    }
+    return array;
+  }
+  // Time Complexity: O(n)
+  // Space Complexity: O(1)
+
+  siftDown(currentIdx, endIdx, heap) {
+    let childOneIdx = currentIdx * 2 + 1;
+    while (childOneIdx <= endIdx) {
+      const childTwoIdx =
+        currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+      let idxToSwap;
+      if (childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) {
+        idxToSwap = childTwoIdx;
+      } else {
+        idxToSwap = childOneIdx;
+      }
+      if (heap[idxToSwap] < heap[currentIdx]) {
+        this.swap(currentIdx, idxToSwap, heap);
+        currentIdx = idxToSwap;
+        childOneIdx = currentIdx * 2 + 1;
+      } else {
+        return;
+      }
+    }
+  }
+  // Time Complexity: O(log n)
+  // Space Complexity: O(1)
+
+  siftUp(currentIdx, heap) {
+    let parentIdx = Math.floor((currentIdx - 1) / 2);
+    while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+      this.swap(currentIdx, parentIdx, heap);
+      currentIdx = parentIdx;
+      parentIdx = Math.floor((currentIdx - 1) / 2);
+    }
+  }
+  // Time Complexity: O(log n)
+  // Space Complexity: O(1)
+
+  peek() {
+    return this.heap[0];
+  }
+  // Time Complexity: O(1)
+  // Space Complexity: O(1)
+
+  remove() {
+    this.swap(0, this.heap.length - 1, this.heap);
+    const valueToRemove = this.heap.pop();
+    this.siftDown(0, this.heap.length - 1, this.heap);
+    return valueToRemove;
+  }
+  // Time Complexity: O(log n)
+  // Space Complexity: O(1)
+
+  insert(value) {
+    this.heap.push(value);
+    this.siftUp(this.heap.length - 1, this.heap);
+  }
+  // Time Complexity: O(log n)
+  // Space Complexity: O(1)
+
+  swap(i, j, heap) {
+    const temp = heap[j];
+    heap[j] = heap[i];
+    heap[i] = temp;
   }
 }
