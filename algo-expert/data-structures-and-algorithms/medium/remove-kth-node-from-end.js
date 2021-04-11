26ebf0e26ad8a10dc93d865ad01caf9e27fae4fa
Raymond Hunce <hunce18c@gmail.com>
2021-04-11 17:40:42 -0400

    Implement source solution for remove-kth-node-from-end

--- algo-expert/data-structures-and-algorithms/medium/remove-kth-node-from-end.js
+++ algo-expert/data-structures-and-algorithms/medium/remove-kth-node-from-end.js
@@ -53,3 +53,25 @@ function removeKthNodeFromEnd(head, k) {
   }
 }
 // O(n) time | O(n) space
+
+// SOURCE SOLUTION
+function removeKthNodeFromEnd(head, k) {
+  let counter = 1;
+  let first = head;
+  let second = head;
+  while (counter <= k) {
+    second = second.next;
+    counter++;
+  }
+  if (second === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+    return;
+  }
+  while (second.next !== null) {
+    second = second.next;
+    first = first.next;
+  }
+  first.next = first.next.next;
+}
+// O(n) time | O(1) space
