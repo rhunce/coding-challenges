c35a61ec98b30fd2c3a29f29121ec00524feffc6
Raymond Hunce <hunce18c@gmail.com>
2021-04-11 09:22:53 -0400

    Implement solution attempt for linked-list-construction

--- algo-expert/data-structures-and-algorithms/medium/linked-list-construction.js
+++ algo-expert/data-structures-and-algorithms/medium/linked-list-construction.js
@@ -1,8 +1,16 @@
 /*
+Write a DoublyLinkedList class that has a head and a tail, both of which point to either a linked list Node or null. The class should support:
 
+ - Setting the head and tail of the linked list;
+ - Inserting nodes before and after other nodes as well as at given positions (the position of the head node is 1);
+ - Removing given nodes and removing nodes with given values;
+ - Searching for nodes with given values.
+
+Note that the setHead, setTail, insertBefore, insertAfter, insertAtPosition, and remove methods all take in actual Nodes as input parameters - not integers (except for insertAtPosition, which also takes in an integer representing the position); this means that you don't need to create any new Nodes in these methods. The input nodes can be either stand-alone nodes or nodes that are already in the linked list. If they're nodes that are already in the linked list, the methods will effectively be moving the nodes within the linked list, so your code will have to defensively handle this scenario.
+
+Each node has an integer value as well as a prev node and a next node, both of which can point to either another node or null.
 */
 
-// MY 45 MIN ATTEMPT
 // This is an input class. Do not edit.
 class Node {
   constructor(value) {
@@ -12,7 +20,7 @@ class Node {
   }
 }
 
-// Feel free to add new properties and methods to the class.
+// MY 45 MIN ATTEMPT
 class DoublyLinkedList {
   constructor() {
     this.head = null;
