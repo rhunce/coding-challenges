6d37bf0ae9809492faa802f71e8baba41ed70fdf
Raymond Hunce <hunce18c@gmail.com>
2021-04-12 18:57:40 -0400

    Implement source solutions for powerset

--- algo-expert/data-structures-and-algorithms/medium/powerset.js
+++ algo-expert/data-structures-and-algorithms/medium/powerset.js
@@ -31,3 +31,36 @@ function getAllSubsets(
     subset.pop();
   }
 }
+
+// SOURCE SOLUTION 1
+function powerset(array, idx = null) {
+  if (idx === null) {
+    idx = array.length - 1;
+  }
+  if (idx < 0) {
+    return [[]];
+  }
+  const ele = array[idx];
+  const subsets = powerset(array, idx - 1);
+  const length = subsets.length;
+  for (let i = 0; i < length; i++) {
+    const currentSubset = subsets[i];
+    subsets.push(currentSubset.concat(ele));
+  }
+  return subsets;
+}
+// O(n*2^n) time | O(n*2^n) space
+
+// SOURCE SOLUTION 2
+function powerset(array, idx = null) {
+  const subsets = [[]];
+  for (const ele of array) {
+    const length = subsets.length;
+    for (let i = 0; i < length; i++) {
+      const currentSubset = subsets[i];
+      subsets.push(currentSubset.concat(ele));
+    }
+  }
+  return subsets;
+}
+// O(n*2^n) time | O(n*2^n) space
