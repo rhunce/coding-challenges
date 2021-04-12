d13c750a82451a947fa8457027526f8f5f8ec024
Raymond Hunce <hunce18c@gmail.com>
2021-04-12 16:02:42 -0400

    Implement source solutions for permutations

--- algo-expert/data-structures-and-algorithms/medium/permutations.js
+++ algo-expert/data-structures-and-algorithms/medium/permutations.js
@@ -2,7 +2,7 @@
 
 */
 
-// MY SOLUTION
+// MY SOLUTION (22 min)
 function getPermutations(array) {
   if (array.length === 0) {
     return [];
@@ -26,4 +26,51 @@ function getAllPermutations(numbers, container, permutation = []) {
       permutation.pop();
     }
   }
+}
+
+// SOURCE SOLUTION 1
+function getPermutations(array) {
+  const permutations = [];
+  permutationsHelper(array, [], permutations);
+  return permutations;
+}
+// Upper bound: O(n! n^2) time | O(n! n) space
+// Roughly: O(n! n) time | O(n! n) space
+
+function permutationsHelper(array, currentPermutation, permutations) {
+  if (!array.length && currentPermutation.length) {
+    permutations.push(currentPermutation);
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      const newArray = array.slice(0, i).concat(array.slice(i + 1));
+      const newPermutation = currentPermutation.concat([array[i]]);
+      permutationsHelper(newArray, newPermutation, permutations);
+    }
+  }
+}
+
+// SOURCE SOLUTION 2
+function getPermutations(array) {
+  const permutations = [];
+  permutationsHelper(0, array, permutations);
+  return permutations;
+}
+// O(n! n) time | O(n! n) space
+
+function permutationsHelper(i, array, permutations) {
+  if (i === array.length - 1) {
+    permutations.push(array.slice());
+  } else {
+    for (let j = i; j < array.length; j++) {
+      swap(i, j, array);
+      permutationsHelper(i + 1, array, permutations);
+      swap(i, j, array);
+    }
+  }
+}
+
+function swap(i, j, array) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
 }
