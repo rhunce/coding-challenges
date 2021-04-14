140360f3e2be4f7d508971f64c65fe7a16da74db
Raymond Hunce <hunce18c@gmail.com>
2021-04-14 15:14:26 -0400

    Implement source solution for min-max-stack-construction

--- min-max-stack-construction.js
+++ min-max-stack-construction.js
@@ -62,3 +62,47 @@ class MinMaxStack {
     return this.max[this.max.length - 1];
   }
 }
+
+// SOURCE SOLUTION
+class MinMaxStack {
+  constructor() {
+    this.minMaxStack = [];
+    this.stack = [];
+  }
+
+  // O(1) time and space
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  // O(1) time and space
+  pop() {
+    this.minMaxStack.pop();
+    return this.stack.pop();
+  }
+
+  // O(1) time and space
+  push(number) {
+    const newMinMax = {
+      min: number,
+      max: number,
+    };
+    if (this.minMaxStack.length) {
+      const lastMinMax = this.minMaxStack[this.minMaxStack.length - 1];
+      newMinMax.min = Math.min(lastMinMax.min, number);
+      newMinMax.max = Math.max(lastMinMax.max, number);
+    }
+    this.minMaxStack.push(newMinMax);
+    this.stack.push(number);
+  }
+
+  // O(1) time and space
+  getMin() {
+    return this.minMaxStack[this.minMaxStack.length - 1].min;
+  }
+
+  // O(1) time and space
+  getMax() {
+    return this.minMaxStack[this.minMaxStack.length - 1].max;
+  }
+}
