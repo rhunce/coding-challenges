680f60243f6505078a4f7fa77a7cf6c616cae6bb
Raymond Hunce <hunce18c@gmail.com>
2021-04-23 15:47:57 -0400

    Implement source solution for reverse-words-in-string

--- algo-expert/data-structures-and-algorithms/medium/reverse-words-in-string.js
+++ algo-expert/data-structures-and-algorithms/medium/reverse-words-in-string.js
@@ -32,3 +32,35 @@ function reverseWordsInString(string) {
   return reversedParts.join('');
 }
 // O(n) time | O(n) space
+
+// SOURCE SOLUTION
+function reverseWordsInString(string) {
+  const characters = [];
+  for (const char of string) {
+    characters.push(char);
+  }
+  reverseListRange(characters, 0, characters.length - 1);
+
+  let startOfWord = 0;
+  while (startOfWord < characters.length) {
+    let endOfWord = startOfWord;
+    while (endOfWord < characters.length && characters[endOfWord] !== ' ') {
+      endOfWord++;
+    }
+
+    reverseListRange(characters, startOfWord, endOfWord - 1);
+    startOfWord = endOfWord + 1;
+  }
+  return characters.join('');
+}
+
+function reverseListRange(list, start, end) {
+  while (start < end) {
+    const temp = list[start];
+    list[start] = list[end];
+    list[end] = temp;
+    start++;
+    end--;
+  }
+}
+// O(n) time | O(n) space
