53bdbeec1fb73e9856ec344a7007d44edc4cef1d
Raymond Hunce <hunce18c@gmail.com>
2021-04-25 10:28:09 -0400

    Implement source solution for suffix-trie-construction

--- algo-expert/data-structures-and-algorithms/medium/suffix-trie-construction.js
+++ algo-expert/data-structures-and-algorithms/medium/suffix-trie-construction.js
@@ -8,6 +8,8 @@ Write a SuffixTrie class for a Suffix-Trie-like data structure. The class should
 Note that every string added to the trie should end with the special endSymbol character: '*'.
 */
 
+// MY SOLUTION ATTEMPT (INCORRECT)
+// MY INCORRECT SOLUTION ATTEMPT
 class SuffixTrie {
   constructor(string) {
     this.root = {};
@@ -15,8 +17,6 @@ class SuffixTrie {
     this.populateSuffixTrieFrom(string);
   }
 
-  // MY SOLUTION ATTEMPT (INCORRECT)
-  // MY INCORRECT SOLUTION ATTEMPT
   populateSuffixTrieFrom(string) {
     for (let i = 0; i < string.length; i++) {
       let currentNode = this.root;
@@ -56,5 +56,45 @@ class SuffixTrie {
       }
     }
     return false;
+  }
+}
+
+// SOURCE SOLUTION
+class SuffixTrie {
+  constructor(string) {
+    this.root = {};
+    this.endSymbol = '*';
+    this.populateSuffixTrieFrom(string);
+  }
+
+  // O(n^2) time | O(n^2) space
+  populateSuffixTrieFrom(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.insertSubstringStartingAt(i, string);
+    }
+  }
+
+  insertSubstringStartingAt(i, string) {
+    let node = this.root;
+    for (let j = i; j < string.length; j++) {
+      const letter = string[j];
+      if (!(letter in node)) {
+        node[letter] = {};
+      }
+      node = node[letter];
+    }
+    node[this.endSymbol] = true;
+  }
+
+  // O(m) time | O(1) space
+  contains(string) {
+    let node = this.root;
+    for (const letter of string) {
+      if (!(letter in node)) {
+        return false;
+      }
+      node = node[letter];
+    }
+    return this.endSymbol in node;
   }
 }
