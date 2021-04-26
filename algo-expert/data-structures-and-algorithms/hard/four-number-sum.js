dc5b79a1e4c50e4b0768c5bb7d04fe40ce5fbef5
Raymond Hunce <hunce18c@gmail.com>
2021-04-26 16:22:35 -0400

    Implement source force solution for four-number-sum

--- algo-expert/data-structures-and-algorithms/hard/four-number-sum.js
+++ algo-expert/data-structures-and-algorithms/hard/four-number-sum.js
@@ -4,7 +4,7 @@ Write a function that takes in a non-empty array of distinct integers and an int
 If no four numbers sum up to the target sum, the function should return an empty array.
 */
 
-// BRUTE FORCE
+// BRUTE FORCE (8 min)
 function fourNumberSum(array, targetSum) {
   const validQuadruplets = [];
   for (let i = 0; i < array.length; i++) {
@@ -22,3 +22,31 @@ function fourNumberSum(array, targetSum) {
   return validQuadruplets;
 }
 // O(n^4) time | O(n) space
+
+// SOURCE SOLUTION
+function fourNumberSum(array, targetSum) {
+  const allPairSums = {};
+  const quadruplets = [];
+  for (let i = 1; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      const currentSum = array[i] + array[j];
+      const difference = targetSum - currentSum;
+      if (difference in allPairSums) {
+        for (const pair of allPairSums[difference]) {
+          quadruplets.push(pair.concat([array[i], array[j]]));
+        }
+      }
+    }
+    for (let k = 0; k < i; k++) {
+      const currentSum = array[i] + array[k];
+      if (!(currentSum in allPairSums)) {
+        allPairSums[currentSum] = [[array[k], array[i]]];
+      } else {
+        allPairSums[currentSum].push([array[k], array[i]]);
+      }
+    }
+  }
+  return quadruplets;
+}
+// Average: O(n^2) time | O(n^2) space
+// Worst: O(n^3) time | O(n^2) space
